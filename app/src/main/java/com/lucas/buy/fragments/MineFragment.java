459a08320a7fc385fb1f7de7f46a30868ee41339
lucas <mlpabcd@sina.com>
2017-07-12 22:47:56 +0800

    测试通过Volley加载服务器上的图片

--- app/src/main/java/com/lucas/buy/fragments/MineFragment.java
+++ app/src/main/java/com/lucas/buy/fragments/MineFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.lucas.buy.R;
+import com.lucas.buy.actiivities.MyApplication;
+import com.lucas.buy.contents.UserContents;
+import com.lucas.buy.utils.VolleyImageUtils;
 
 /**
  * Created by 111 on 2017/7/7.
@@ -15,11 +20,18 @@ import com.lucas.buy.R;
 
 public class MineFragment extends Fragment {
     private View view;
+    private ImageView img;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.mine_layout,container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        img = (ImageView) view.findViewById(R.id.mine_img);
+        VolleyImageUtils.loadImage(UserContents.imageUrl, img);
     }
 
     @Override
---
+++ app/src/main/java/com/lucas/buy/utils/VolleyImageUtils.java
@@ -0,0 +1,19 @@
+package com.lucas.buy.utils;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.lucas.buy.R;
+import com.lucas.buy.actiivities.MyApplication;
+
+/**
+ * Created by 111 on 2017/7/12.
+ */
+
+public class VolleyImageUtils {
+    public static void loadImage(String url, View view) {
+        ImageLoader.ImageListener listener = ImageLoader.getImageListener((ImageView)view, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
+        MyApplication.getImageLoader().get(url, listener);
+    }
+}
